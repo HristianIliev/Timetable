03c0088c1cd596332501b3564a168e2caddbc8e6
argir-boyadzhiev <56366942+argir-boyadzhiev@users.noreply.github.com>
2022-06-11 12:14:42 +0300

    refactoring

--- dependency.js
+++ dependency.js
@@ -1,5 +1,22 @@
 window.addEventListener('load', start());
 
+// load ->
+//       start ->
+//              createLinkFromString ->
+//                      findIdOfCourse
+//              init ->
+//                      splitGraphs ->
+//                              mark
+//                              sortGroup ->
+//                                      topologicalSort ->
+//                                              topologicalSortHelper
+//                      drawConnectedCourseTabs ->
+//                              findTabWidth
+//                              generateRandomColor
+//                              findCOursePlaceFromId
+//                              drawLink
+//                              drawCourseTab
+
 var canvas;
 
 var offset = 4;
@@ -12,57 +29,71 @@ var defaultFont = "20px Arial";
 var coursesArray;
 var coursesArrayBuffer;
 
-// Topological sorting Start
-function topologicalSortHelper(node, explored, s, group) {
-   explored.add(node);
-   // Marks this node as visited and goes on to the nodes
-   // that are dependent on this node, the edge is node ----> n
-   coursesArray[node].links.forEach(childNode => {
-      if (!explored.has(childNode)) {
-         topologicalSortHelper(childNode, explored, s);
-      }
-   });
-   // All dependencies are resolved for this node, we can now add
-   // This to the stack.
-   s.push(node);
+/// Gathers the courses names and dependacies from the data base and initializes their visualisation 
+function start() {
+    coursesArray = new Array();
+    fetch('http://localhost/timetable/api/get-courses.php')
+        .then(res => res.json())
+        .then(courses => {
+            // Courses names
+            let iter = 0;
+            courses.forEach(course => {
+                coursesArray[iter] = new Object();
+                coursesArray[iter].id = iter;
+                coursesArray[iter].name = course.title;
+                coursesArray[iter].links = new Array();
+                coursesArray[iter].backLinks = new Array();
+                iter++;
+            });
+
+            // Courses links
+            iter = 0;
+            courses.forEach(course => {
+
+                createLinkFromString(course.dependencies, iter);
+                iter++;
+            });
+
+            init();
+        });
 }
 
-function topologicalSort(group) {
-   // Create a Stack to keep track of all elements in sorted order
-   let s = [];
-   let explored = new Set();
-
-   // For every unvisited node in our graph, call the helper.
-   group.forEach(node => {
-      if (!explored.has(node)) {
-         topologicalSortHelper(node, explored, s);
-      }
-   });
-
-   return s;
-}
-// Topological sorting End
-
-function sortGroup(group) {
-    let reverseStack = topologicalSort(group);
-    while(reverseStack.length) {
-        coursesArrayBuffer[coursesArrayBuffer.length] = coursesArray[reverseStack.pop()];
+/// /Add links to coursesArray.links based on the dependancies string
+function createLinkFromString(dependenciesString, parentId) {
+    let dependencies = dependenciesString.split(";");
+    for(let i = 0; i < dependencies.length; i++) {
+        let childId = findIdOfCourse(dependencies[i]);
+        if(childId != -1) {
+            coursesArray[parentId].links[coursesArray[parentId].links.length] = childId;
+            coursesArray[childId].backLinks[coursesArray[childId].backLinks.length] = parentId;
+        }
     }
 }
 
-function mark(markers, markedCourses, currentId, groupId) {
-    if(markedCourses.has(currentId))
-        return;
-    markedCourses.add(currentId);
-    markers[currentId] = groupId;
-    for(let i = 0; i < coursesArray[currentId].links.length; i++) {
-        mark(markers, markedCourses, coursesArray[currentId].links[i], groupId);
+/// Return id of a course based on name
+function findIdOfCourse(name) {
+    for(let i = 0; i < coursesArray.length; i++) {
+        if(coursesArray[i].name == name) {
+            return coursesArray[i].id;
+        }
     }
-    for(let i = 0; i < coursesArray[currentId].backLinks.length; i++) {
-        mark(markers, markedCourses, coursesArray[currentId].backLinks[i], groupId);
-    }
+    return -1;
 }
 
+/// Prepares the courses and the canvas elemnt and inisializes the drawing
+function init() {
+    splitGraphs();
+
+    let canvasElement = document.getElementById('drawingCanvas');
+    canvasElement.width = 700;
+    canvasElement.height = coursesArray.length * (tabHeight + 5 * offset) + tabHeight * 2;
+
+    canvas = canvasElement.getContext("2d");
+    drawConnectedCourseTabs(); 
+}
+
+/// Splits the courses into groups of connected courses by dependancies
+/// From one big graph to a forest of connected graphs
 function splitGraphs() {
     let markers = new Array();
     let markedCourses = new Set();
@@ -96,73 +127,57 @@ function splitGraphs() {
     coursesArray = coursesArrayBuffer;
 }
 
-function findIdOfCourse(name) {
-    for(let i = 0; i < coursesArray.length; i++) {
-        if(coursesArray[i].name == name) {
-            return coursesArray[i].id;
-        }
+/// Helper function that is used to track if a course has already been grouped
+function mark(markers, markedCourses, currentId, groupId) {
+    if(markedCourses.has(currentId))
+        return;
+    markedCourses.add(currentId);
+    markers[currentId] = groupId;
+    for(let i = 0; i < coursesArray[currentId].links.length; i++) {
+        mark(markers, markedCourses, coursesArray[currentId].links[i], groupId);
     }
-    return -1;
-}
-
-function createLinkFromString(dependenciesString, parentId) {
-    let dependencies = dependenciesString.split(";");
-    for(let i = 0; i < dependencies.length; i++) {
-        let childId = findIdOfCourse(dependencies[i]);
-        if(childId != -1) {
-            coursesArray[parentId].links[coursesArray[parentId].links.length] = childId;
-            coursesArray[childId].backLinks[coursesArray[childId].backLinks.length] = parentId;
-        }
+    for(let i = 0; i < coursesArray[currentId].backLinks.length; i++) {
+        mark(markers, markedCourses, coursesArray[currentId].backLinks[i], groupId);
     }
 }
 
-function start() {
-    coursesArray = new Array();
-    fetch('http://localhost/timetable/api/get-courses.php')
-        .then(res => res.json())
-        .then(courses => {
-            // Courses names
-            let iter = 0;
-            courses.forEach(course => {
-                coursesArray[iter] = new Object();
-                coursesArray[iter].id = iter;
-                coursesArray[iter].name = course.title;
-                coursesArray[iter].links = new Array();
-                coursesArray[iter].backLinks = new Array();
-                iter++;
-            });
-
-            // Courses links
-            iter = 0;
-            courses.forEach(course => {
-
-                createLinkFromString(course.dependencies, iter);
-                iter++;
-            });
-
-            init();
-        });
-}
-
-function init() {
-    splitGraphs();
-
-    let canvasElement = document.getElementById('drawingCanvas');
-    canvasElement.width = 700;
-    canvasElement.height = coursesArray.length * (tabHeight + 5 * offset) + tabHeight * 2;
-
-    canvas = canvasElement.getContext("2d");
-    drawConnectedCourseTabs(); 
-}
-
-function findCoursePlaceFromId(id) {
-    let place = 0;
-    for(let i = 0; i < coursesArray.length; i++) {
-        if(coursesArray[i].id == id) {
-            return i
-        }
+/// Calls the topological sort functions that sort the connected graph
+function sortGroup(group) {
+    let reverseStack = topologicalSort(group);
+    while(reverseStack.length) {
+        coursesArrayBuffer[coursesArrayBuffer.length] = coursesArray[reverseStack.pop()];
     }
-    return -1;
+}
+
+/// Topological sorting is used so that the dependancies are always from the upper to the lower course in the column
+/// Topological sorting Start
+function topologicalSort(group) {
+    // Create a Stack to keep track of all elements in sorted order
+    let s = [];
+    let explored = new Set();
+ 
+    // For every unvisited node in our graph, call the helper.
+    group.forEach(node => {
+       if (!explored.has(node)) {
+          topologicalSortHelper(node, explored, s);
+       }
+    });
+ 
+    return s;
+ }
+
+function topologicalSortHelper(node, explored, s, group) {
+   explored.add(node);
+   // Marks this node as visited and goes on to the nodes
+   // that are dependent on this node, the edge is node ----> n
+   coursesArray[node].links.forEach(childNode => {
+      if (!explored.has(childNode)) {
+         topologicalSortHelper(childNode, explored, s);
+      }
+   });
+   // All dependencies are resolved for this node, we can now add
+   // This to the stack.
+   s.push(node);
 }
 
 /// Draws the tabs of the different courses in a vertical order and connects
@@ -192,21 +207,38 @@ function drawConnectedCourseTabs() {
     }
 }
 
-/// Prints the name of the course in all caps and a rectangular background behind it
-/// xPos and yPos - the coordinates of the upper left corner
-/// name - the name of the course
-/// width - the maximal width in pixels that the rectangle background is supposed to have
-function drawCourceTab(xPos, yPos, name, width, bgColor) {
-    // Background
-    canvas.fillStyle = bgColor;
-    canvas.fillRect(xPos, yPos - tabHeight, width, tabHeight);
-
-    // Text
-    canvas.fillStyle = defaultColor;
-    canvas.font = defaultFont;
-    canvas.fillText(name.toUpperCase() , xPos + offset, yPos - offset, width - offset);
+/// Returns the width in pixels of the rectangle background of the widest name of course
+function findTabWidth() {
+    let width = 0;
+    for(let i = 0; i < coursesArray.length; i++) {
+        if((coursesArray[i].name.length * pixelsPerLetter + offset * 2) > width) {
+            width = coursesArray[i].name.length * pixelsPerLetter + offset * 2;
+        }
+    }
+    return width;
 }
 
+function generateRandomColor(){
+    let maxVal = 0x444444;
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber) + 0xAAAAAB;
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+
+/// Returns the place of a course in the final column by its id
+function findCoursePlaceFromId(id) {
+    let place = 0;
+    for(let i = 0; i < coursesArray.length; i++) {
+        if(coursesArray[i].id == id) {
+            return i
+        }
+    }
+    return -1;
+}
+
+/// Draw an arc in canvas connecting two courses
 function drawLink(xPos, yPosBeg, yPosEnd, isLeft, color) {
     if(yPosBeg > yPosEnd) {
         let s = yPosEnd;
@@ -234,22 +266,17 @@ function drawLink(xPos, yPosBeg, yPosEnd, isLeft, color) {
     canvas.stroke()
 }
 
-/// Returns the width in pixels of the rectangle background of the widest name of course
-function findTabWidth() {
-    let width = 0;
-    for(let i = 0; i < coursesArray.length; i++) {
-        if((coursesArray[i].name.length * pixelsPerLetter + offset * 2) > width) {
-            width = coursesArray[i].name.length * pixelsPerLetter + offset * 2;
-        }
-    }
-    return width;
-}
+/// Prints the name of the course in all caps and a rectangular background behind it
+/// xPos and yPos - the coordinates of the upper left corner
+/// name - the name of the course
+/// width - the maximal width in pixels that the rectangle background is supposed to have
+function drawCourceTab(xPos, yPos, name, width, bgColor) {
+    // Background
+    canvas.fillStyle = bgColor;
+    canvas.fillRect(xPos, yPos - tabHeight, width, tabHeight);
 
-function generateRandomColor(){
-    let maxVal = 0x444444;
-    let randomNumber = Math.random() * maxVal; 
-    randomNumber = Math.floor(randomNumber) + 0xAAAAAB;
-    randomNumber = randomNumber.toString(16);
-    let randColor = randomNumber.padStart(6, 0);   
-    return `#${randColor.toUpperCase()}`
+    // Text
+    canvas.fillStyle = defaultColor;
+    canvas.font = defaultFont;
+    canvas.fillText(name.toUpperCase() , xPos + offset, yPos - offset, width - offset);
 }
